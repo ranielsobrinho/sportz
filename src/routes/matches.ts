c95ab7d918bc2a31ba235820970dca9cdb47e584
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2026-04-02 10:58:48 -0300

    feat: implement the websocket server

--- src/routes/matches.ts
+++ src/routes/matches.ts
@@ -76,6 +76,10 @@ matchRouter.post("/", async (req: Request, res: Response) => {
       })
       .returning();
 
+    if (res.app.locals.broadcastMatchCreated) {
+      res.app.locals.broadcastMatchCreated(event);
+    }
+
     res.status(201).json({ data: event });
   } catch (error) {
     return res.status(500).json({
